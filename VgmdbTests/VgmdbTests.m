1aeaf42c7ddf96e97c47d6e4855d7198bfcb2f59
Bilal Hussain <bhterra@gmail.com>
2012-07-28 00:41:44 +0100

    Got testing working

---
+++ VgmdbTests/VgmdbTests.m
@@ -0,0 +1,34 @@
+//
+//  VgmdbTests.m
+//  VgmdbTests
+//
+//  Created by Bilal Syed Hussain on 28/07/2012.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VgmdbTests : SenTestCase
+
+@end
+
+@implementation VgmdbTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+
+}
+
+@end
