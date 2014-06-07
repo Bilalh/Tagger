0d12c73d80984b5e873bd15b53709bcb40e76e1f
Bilal Hussain <bhterra@gmail.com>
2014-06-08 00:31:41 +0100

    Since we are using AVfoundation use NSStrings constraints

--- Classes/Taglib/Fields.h
+++ Classes/Taglib/Fields.h
@@ -9,66 +9,63 @@
 #ifndef FIELDS_HEADERR
 #define FIELDS_HEADERR
 
-#include <tstring.h> 
+#import <Foundation/Foundation.h>
 
 // Contains the identifier for the fields of each mediatype
 namespace Fields {
 	
 	namespace MP4 {
-		using TagLib::String;
-		String const ARTIST                        = "\251ART";
-		String const ALBUM                         = "\251alb";
-		String const TITLE                         = "\251nam";
-		String const TRACK_NUMBER                  = "trkn";
-		String const YEAR                          = "\251day";
-		String const GENRE                         = "\251gen";
-		String const COMMENT                       = "\251cmt";
-		String const ALBUM_ARTIST                  = "aART";
-		String const COMPOSER                      = "\251wrt";
-		String const GROUPING                      = "\251grp";
-		String const DISC_NUMBER                   = "disk";
-		String const COVER                         = "covr";
-		String const BPM                           = "tmpo";
-		String const MUSICBRAINZ_ARTISTID          = "----:com.apple.iTunes:MusicBrainz Artist Id";
-		String const MUSICBRAINZ_RELEASEID         = "----:com.apple.iTunes:MusicBrainz Album Id";
-		String const MUSICBRAINZ_RELEASE_ARTISTID  = "----:com.apple.iTunes:MusicBrainz Album Artist Id";
-		String const MUSICBRAINZ_TRACK_ID          = "----:com.apple.iTunes:MusicBrainz Track Id";
-		String const MUSICBRAINZ_DISC_ID           = "----:com.apple.iTunes:MusicBrainz Disc Id";
-		String const MUSICIP_ID                    = "----:com.apple.iTunes:MusicIP PUID";
-		String const AMAZON_ID                     = "----:com.apple.iTunes:ASIN";
-		String const MUSICBRAINZ_RELEASE_STATUS    = "----:com.apple.iTunes:MusicBrainz Album Status";
-		String const MUSICBRAINZ_RELEASE_TYPE      = "----:com.apple.iTunes:MusicBrainz Album Type";
-		String const MUSICBRAINZ_RELEASE_COUNTRY   = "----:com.apple.iTunes:MusicBrainz Album Release Country";
-		String const LYRICS                        = "\251lyr";
-		String const COMPILATION                   = "cpil";
-		String const ARTIST_SORT                   = "soar";
-		String const ALBUM_ARTIST_SORT             = "soaa";
-		String const ALBUM_SORT                    = "soal";
-		String const TITLE_SORT                    = "sonm";
-		String const COMPOSER_SORT                 = "soco";
-		String const ENCODER                       = "\251too";
-		String const ISRC                          = "----:com.apple.iTunes:ISRC";
-		String const BARCODE                       = "----:com.apple.iTunes:BARCODE";
-		String const CATALOG_NO                    = "----:com.apple.iTunes:CATALOGNUMBER";
-		String const RECORD_LABEL                  = "----:com.apple.iTunes:LABEL";
-		String const LYRICIST                      = "----:com.apple.iTunes:LYRICIST";
-		String const CONDUCTOR                     = "----:com.apple.iTunes:CONDUCTOR";
-		String const REMIXER                       = "----:com.apple.iTunes:REMIXER";
-		String const MOOD                          = "----:com.apple.iTunes:MOOD";
-		String const MEDIA                         = "----:com.apple.iTunes:MEDIA";
-		String const URL                           = "----:com.apple.iTunes:URL_OFFICIAL_RELEASE_SITE";
-		String const DISCOGS_RELEASE_SITE_URL      = "----:com.apple.iTunes:URL_DISCOGS_RELEASE_SITE";
-		String const WIKIPEDIA_RELEASE_SITE_URL    = "----:com.apple.iTunes:URL_WIKIPEDIA_RELEASE_SITE";
-		String const OFFICIAL_ARTIST_SITE_URL      = "----:com.apple.iTunes:URL_OFFICIAL_ARTIST_SITE";
-		String const DISCOGS_ARTIST_SITE_URL       = "----:com.apple.iTunes:URL_DISCOGS_ARTIST_SITE";
-		String const WIKIPEDIA_ARTIST_SITE_URL     = "----:com.apple.iTunes:URL_WIKIPEDIA_ARTIST_SITE";
-		String const LANGUAGE                      = "----:com.apple.iTunes:LANGUAGE";
-		String const LYRICS_SITE_URL               = "----:com.apple.iTunes:URL_LYRICS_SITE";
-		String const TOTAL_TRACKS                  = "trkn";
-		String const TOTAL_DISCS                   = "disk";
-        
-        String const RELEASE_DATE                   = "\251day";
-
+        extern NSString* const ARTIST;
+		extern NSString* const ALBUM;
+		extern NSString* const TITLE;
+		extern NSString* const TRACK_NUMBER;
+		extern NSString* const YEAR;
+		extern NSString* const GENRE;
+		extern NSString* const COMMENT;
+		extern NSString* const ALBUM_ARTIST;
+		extern NSString* const COMPOSER;
+		extern NSString* const GROUPING;
+		extern NSString* const DISC_NUMBER;
+		extern NSString* const COVER;
+		extern NSString* const BPM;
+		extern NSString* const MUSICBRAINZ_ARTISTID;
+		extern NSString* const MUSICBRAINZ_RELEASEID;
+		extern NSString* const MUSICBRAINZ_RELEASE_ARTISTID;
+		extern NSString* const MUSICBRAINZ_TRACK_ID;
+		extern NSString* const MUSICBRAINZ_DISC_ID;
+		extern NSString* const MUSICIP_ID;
+		extern NSString* const AMAZON_ID;
+		extern NSString* const MUSICBRAINZ_RELEASE_STATUS;
+		extern NSString* const MUSICBRAINZ_RELEASE_TYPE;
+		extern NSString* const MUSICBRAINZ_RELEASE_COUNTRY;
+		extern NSString* const LYRICS;
+		extern NSString* const COMPILATION;
+		extern NSString* const ARTIST_SORT;
+		extern NSString* const ALBUM_ARTIST_SORT;
+		extern NSString* const ALBUM_SORT;
+		extern NSString* const TITLE_SORT;
+		extern NSString* const COMPOSER_SORT;
+		extern NSString* const ENCODER;
+		extern NSString* const ISRC;
+		extern NSString* const BARCODE;
+		extern NSString* const CATALOG_NO;
+		extern NSString* const RECORD_LABEL;
+		extern NSString* const LYRICIST;
+		extern NSString* const CONDUCTOR;
+		extern NSString* const REMIXER;
+		extern NSString* const MOOD;
+		extern NSString* const MEDIA;
+		extern NSString* const URL;
+		extern NSString* const DISCOGS_RELEASE_SITE_URL;
+		extern NSString* const WIKIPEDIA_RELEASE_SITE_URL;
+		extern NSString* const OFFICIAL_ARTIST_SITE_URL;
+		extern NSString* const DISCOGS_ARTIST_SITE_URL;
+		extern NSString* const WIKIPEDIA_ARTIST_SITE_URL;
+		extern NSString* const LANGUAGE;
+		extern NSString* const LYRICS_SITE_URL;
+		extern NSString* const TOTAL_TRACKS;
+		extern NSString* const TOTAL_DISCS;
+        extern NSString* const RELEASE_DATE;
 	}
 	
 	namespace MPEG {
